5aa8b005ee7f13295fe1bf2ed1b9417dc35aa155
Bharath Andukuri <100148753+bharathandukuri@users.noreply.github.com>
2026-09-15 10:40:39 +0530

    Add Easy Java Solution for 169. Majority Element from LeetCode

---
+++ LeetCode/Java/Easy/169. Majority Element/Solution.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int num = nums[0];
+        int count = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (num == nums[i]) {
+                count++;
+            } else {
+                count--;
+                if (count == 0) {
+                    num = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return num;
+    }
+}
