dd008ac97c3bc3520355867d5b565f75aa3e1761
Bharath Andukuri <100148753+bharathandukuri@users.noreply.github.com>
2026-09-15 10:43:24 +0530

    Add Hard Java Solution for 41. First Missing Positive from LeetCode

---
+++ LeetCode/Patterns/Hash Map & Hashing/41. First Missing Positive/Solution.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        int i = 0;
+        while (i < n) {
+            if (nums[i] <= 0 || nums[i] > n) {
+                i++;
+                continue;
+            }
+
+            if (nums[nums[i] - 1] != nums[i]) {
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            } else {
+                i++;
+            }
+        }
+
+        for (i = 1; i <= n; i++) {
+            if (nums[i - 1] != i) {
+                return i;
+            }
+        }
+        return n + 1;
+    }
+}
