e706e5ac79d3c1f47465888478c6b0c7ba2ca158
Bharath Andukuri <100148753+bharathandukuri@users.noreply.github.com>
2026-09-16 09:23:58 +0530

    Add Hard Java Solution for 1766. Minimum Number of Removals to Make Mountain Array from LeetCode

---
+++ LeetCode/Patterns/Dynamic Programming/1766. Minimum Number of Removals to Make Mountain Array/Solution.java
@@ -0,0 +1,58 @@
+class Solution {
+    public int minimumMountainRemovals(int[] nums) {
+        int[] lis = lengthOfLIS(nums);
+        int[] lisR = lengthOfLISRev(nums);
+
+        int maxMountain = 0;
+        for (int i = 1; i < nums.length - 1; i++) {
+            if (lis[i] > 1 && lisR[i] > 1) {
+                int mountainLength = lis[i] + lisR[i] - 1;
+                maxMountain = Math.max(maxMountain, mountainLength);
+            }
+        }
+        return nums.length - maxMountain;
+    }
+
+    public int[] lengthOfLIS(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+        int result = 1;
+
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+        }
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i - 1; j >= 0; j--) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+
+        return dp;
+    }
+
+
+    public int[] lengthOfLISRev(int[] nums) {
+        int n = nums.length;
+        int[] dp = new int[n + 1];
+        int result = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+            dp[i] = 1;
+        }
+
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[j] < nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            result = Math.max(result, dp[i]);
+        }
+
+        return dp;
+    }
+}
