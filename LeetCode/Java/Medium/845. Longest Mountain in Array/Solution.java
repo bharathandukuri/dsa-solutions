b229ce7b2051ee26759b10b9faeebe6ac63c1138
Bharath Andukuri <100148753+bharathandukuri@users.noreply.github.com>
2026-09-16 10:20:33 +0530

    Add Medium Java Solution for 845. Longest Mountain in Array from LeetCode

--- LeetCode/Java/Medium/845. Longest Mountain in Array/Solution.java
+++ LeetCode/Java/Medium/845. Longest Mountain in Array/Solution.java
@@ -1,10 +1,27 @@
 class Solution {
     public int longestMountain(int[] nums) {
+        int longest = 0;
+        for (int i = 1; i < nums.length - 1; i++) {
+            longest = Math.max(longest, lengthOfMountain(nums, i));
+        }
+
+        return longest;
     }
 
     private int lengthOfMountain(int[] nums, int peak) {
         int i = peak - 1;
         int j = peak + 1;
-        if ()
+        while (i >= 0 && nums[i] < nums[i + 1]) {
+            i--;
+        }
+        while (j < nums.length && nums[j] > nums[j - 1]) {
+            j++;
+        }
+        int diff = j - i - 1;
+
+        if (diff <= 2) {
+            return 0;
+        }
+        return diff + 2;
     }
 }
